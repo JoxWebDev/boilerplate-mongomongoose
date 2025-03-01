5c4c35cece9c86394178d3cfd0c1f722916103fd
Júlia Castro Meireles <jujucastro2703@gmail.com>
2025-03-01 13:23:18 +0000

    fix: error

--- myApp.js
+++ myApp.js
@@ -1,5 +1,18 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+// Check that MONGO_URI is loaded
+console.log(process.env.MONGO_URI);
+
+const dbURI = process.env.MONGO_URI;
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Successfully connected to the database');
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+  });
 
 let Person;
 
